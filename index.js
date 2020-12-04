bd0856c65890641959ddca09b35fcf3235ba25a5
Aryan Kapoor <64773763+Aryankpoor@users.noreply.github.com>
2020-12-04 22:22:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,5 @@ function w3_close() {
   document.getElementById("mySidebar").style.display = "none";	
   document.getElementById("myOverlay").style.display = "none";	
 }
+
+console.log("ecommerce site")
